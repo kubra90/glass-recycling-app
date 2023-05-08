c8bc5f338d2664a1c4cada2912988bdd21358638
kubra <kubracetin.ist@gmail.com>
2023-05-08 16:16:25 -0400

    comment

--- Desktop/recycling app/java/src/main/java/com/techelevator/Application.java
+++ Desktop/recycling app/java/src/main/java/com/techelevator/Application.java
@@ -3,7 +3,7 @@ package com.techelevator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//comment
+// new comment
 @SpringBootApplication
 public class Application {
 
